fc9343bb1d407717602c9bf97c5ef307303e8bf4
geo-777 <geo.binu.25@gmail.com>
2026-02-05 20:36:43 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// helper function for sending data to googlesheet/backend
+/*
+data = {
+    name : "example name",
+    preference : "male/female/no pref"
+    phoneNo : "9400xxxxxx"
+    email : ""
+}
+*/
+const sendData = (data) => {
+  //api call here
+};
